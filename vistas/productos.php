a97f657b323823cb02e9692e18843731609bca80
JuanDiaz2002 <127808769+JuanDiaz2002@users.noreply.github.com>
2023-03-22 20:36:58 -0600

    Add files via upload

---
+++ vistas/productos.php
@@ -0,0 +1,107 @@
+<?php
+    require('header.php');
+?>
+<div class="content-wrapper">
+      
+        <!--inicio primera fila productos-->
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="small-box bg-info">
+              <div>
+              <img style="width: 270px; height: 270px" src="../img/martillo.jpg"></img>
+              </div>
+              <a href="informacionProd.php" class="small-box-footer">Más informacion <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+
+            <div class="col-1"></div>
+            <div class="small-box bg-info">
+              <div>
+              <img style="width: 270px; height: 270px" src="../img/destornillador.jpg"></img>
+              </div>
+              <a href="informacionProd.php" class="small-box-footer">Más informacion <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+
+            <div class="col-1"></div>
+            <div class="small-box bg-info">
+              <div>
+              <img style="width: 270px; height: 270px" src="../img/llaveajustable.jpg"></img>
+              </div>
+              <a href="informacionProd.php" class="small-box-footer">Más informacion <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+            <!--Cierre primera fila productos-->
+            </div>
+
+            <!--inicio segunda fila-->
+            <div class="row">
+
+            <div class="col-1"></div>
+            <div class="small-box bg-info">
+              <div>
+              <img style="width: 270px; height: 270px" src="../img/segueta.jpg"></img>
+              </div>
+              <a href="informacionProd.php" class="small-box-footer">Más informacion <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+
+            <div class="col-1"></div>
+            <div class="small-box bg-info">
+              <div>
+              <img style="width: 270px; height: 270px" src="../img/palanca.jpg"></img>
+              </div>
+              <a href="informacionProd.php" class="small-box-footer">Más informacion <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+
+            <div class="col-1"></div>
+            <div class="small-box bg-info">
+              <div>
+              <img style="width: 270px; height: 270px" src="../img/pala.jpg"></img>
+              </div>
+              <a href="informacionProd.php" class="small-box-footer">Más informacion <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+        <!--Cierre sefunda fila productos-->
+        </div>
+
+        <div class="row">
+
+          <div class="col-1"></div>
+            <div class="small-box bg-info">
+              <div>
+              <img style="width: 270px; height: 270px" src="../img/rastrillo.jpg"></img>
+              </div>
+              <a href="informacionProd.php" class="small-box-footer">Más informacion <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+
+            <div class="col-1"></div>
+            <div class="small-box bg-info">
+              <div>
+              <img style="width: 270px; height: 270px" src="../img/carretilla.jpg"></img>
+              </div>
+              <a href="informacionProd.php" class="small-box-footer">Más informacion <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+
+            <div class="col-1"></div>
+            <div class="small-box bg-info">
+              <div>
+              <img style="width: 270px; height: 270px" src="../img/cemento.jpg"></img>
+              </div>
+              <a href="informacionProd.php" class="small-box-footer">Más informacion <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+
+        </div>
+
+ 
+            
+
+            
+
+            
+
+
+
+
+
+
+
+
+<?php
+    require('footer.php');
+?>
